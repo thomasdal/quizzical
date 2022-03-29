1c8d68f992d3f4378dc07bf539e01e2cadaedd8c
thomasdal <78147403+thomasdal@users.noreply.github.com>
2022-03-29 15:09:58 +0200

    Update Frontpage.js

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -4,7 +4,7 @@ export default function Frontpage(props){
     return (
         <main className="frontpage">
             <h1 className="frontpage--title">Quizzical</h1>
-            <h3 className="frontpage--desc">Some description if needed</h3>
+            <h3 className="frontpage--desc">- Now with CI/CD Pipeline</h3>
             <button onClick={props.start} className="frontpage--button">Start quiz</button>
         </main>
     )
